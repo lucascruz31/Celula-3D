b99ab22e93d9f8231787c6fb720a69b2fe7b5620
lucascruz31 <ljcruz@correo.unicordoba.edu.co>
2026-05-11 17:10:26 -0500

    v0.1.4

---
+++ app/shared/types/index.ts
@@ -0,0 +1,8 @@
+export interface Perfiles {
+    id: string
+    username: string
+    name: string
+    lastname: string
+    password: string
+    rol: string
+}
---
+++ app/store/perfiles.ts
@@ -0,0 +1,66 @@
+// app/stores/playlists.ts
+// Setup Store de Pinia 3 — playlists con Read y Create
+import { defineStore } from 'pinia'
+import type { Perfiles } from '~/shared/types'
+
+export const usePerfilesStore = defineStore('perfiles', () => {
+    // ── STATE ──────────────────────────────────────────────────────────────────
+    // Se almacenan todas las playlist
+    const perfiles = ref<Perfiles[]>([])
+    // Se almacena la playlist actualmente seleccionada (si hay alguna)
+    const currentPerfil = ref<Perfiles | null>(null)
+    // Indicador de carga para operaciones asíncronas relacionadas con playlists
+    const isLoading = ref(false)
+
+    // ── GETTERS ────────────────────────────────────────────────────────────────
+    // Devuelve el número total de playlists almacenadas
+    const totalPerfiles = computed(() => perfiles.value.length)
+
+    // ── ACTIONS ────────────────────────────────────────────────────────────────
+    // Obtiene todas las playlists desde el backend y las almacena en el estado
+    async function fetchPerfiles() {
+        isLoading.value = true
+        try {
+            perfiles.value = await $fetch<Perfiles[]>('/api/perfiles')
+        } catch (e) {
+            console.error('[PerfilesStore] fetchPerfiles:', e)
+        } finally {
+            isLoading.value = false
+        }
+    }
+
+    /*
+    async function fetchPlaylistById(id: string): Promise<Playlist | null> {
+      try {
+        const playlist = await $fetch<Playlist>(`/api/playlists/${id}`)
+        currentPlaylist.value = playlist
+        return playlist
+      } catch (e) {
+        console.error('[PlaylistsStore] fetchPlaylistById:', e)
+        return null
+      }
+    }*/
+
+    /** Dado un playlist, obtiene los tracks correspondientes del store de tracks */
+    /*function getTracksForPlaylist(playlist: Playlist, allTracks: Track[]): Track[] {
+      return playlist.trackIds
+        .map((id) => allTracks.find((t) => t.id === id))
+        .filter((t): t is Track => !!t)
+    }
+  
+    function setCurrentPlaylist(playlist: Playlist) {
+      currentPlaylist.value = playlist
+    }*/
+
+    // retorna el estado, getters y acciones para ser usados en los componentes y paginas
+    return {
+        perfiles,
+        currentPerfil,
+        isLoading,
+        totalPerfiles,
+        fetchPerfiles,
+        // fetchPlaylistById,
+        // getTracksForPlaylist,
+        // setCurrentPlaylist,
+    }
+})
